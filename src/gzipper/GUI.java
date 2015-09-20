a27952945cdf408c8c0e569058591a90ddc1542f
Matthias <Matthias@192.168.11.2>
2015-09-20 16:03:55 +0200

    initialization

---
+++ src/gzipper/GUI.java
@@ -0,0 +1,423 @@
+/*
+ * Copyright (C) 2015 Matthias Fussenegger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gzipper;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.ButtonGroup;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingConstants;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.text.DefaultCaret;
+
+//CHANGELOG:
+// upgraded to commons-compress 1.10 
+// added support for compressing sub-directories 
+// built GUI for application with advanced functionality
+// added threading capability for GUI responsiveness
+// added Linux compatibility
+public class GUI extends JFrame implements Runnable {
+
+    //ATTRIBUTES
+    private JMenu _fileMenu;
+    private JMenu _helpMenu;
+    private JMenuItem _exitMenuItem;
+    private JMenuItem _aboutMenuItem;
+    private JMenuBar _menuBar;
+    private JLabel _label1;
+    private JTextArea _textOutput;
+    private JButton _startButton;
+    private JButton _abortButton;
+    private JButton _selectButton;
+    private JRadioButton _archiveModeZip;
+    private JRadioButton _archiveModeUnzip;
+    private ButtonGroup _buttonGroup;
+    private JFileChooser _fileChooser;
+    private FileNameExtensionFilter _filter;
+    private JPanel _panel1; //center panel
+    private JPanel _panel2; //southern panel
+    private JPanel _panel3; //northern panel
+
+    private final String INITIAL_PATH; //the decoded standard path of JAR-file
+    private final PauseControl _ps; //to pause GUI-Thread after operation
+    private Zipper _zipper; //associated object for archive operations
+    private Thread _guiThread;
+    private boolean _runFlag;
+
+    protected static boolean _isUnix; //to check whether OS is unix or not
+
+    //CONSTRUCTOR
+    public GUI(String path) {
+        INITIAL_PATH = path;
+        _ps = new PauseControl();
+        initComponents();
+    }
+
+    //METHODS
+    private void initComponents() {
+        /*initialize fields*/
+        _fileMenu = new JMenu();
+        _helpMenu = new JMenu();
+        _exitMenuItem = new JMenuItem("Exit");
+        _aboutMenuItem = new JMenuItem("About");
+        _startButton = new JButton("Start");
+        _abortButton = new JButton("Abort");
+        _selectButton = new JButton("Select...");
+        _archiveModeZip = new JRadioButton("Compress");
+        _archiveModeUnzip = new JRadioButton("Decompress");
+        _buttonGroup = new ButtonGroup();
+        _fileChooser = new JFileChooser();
+        _filter = new FileNameExtensionFilter(".tar.gz", "gz");
+        _menuBar = new JMenuBar();
+        _label1 = new JLabel("Choose ZIP-mode:");
+        _textOutput = new JTextArea();
+        _panel1 = new JPanel();
+        _panel2 = new JPanel();
+        _panel3 = new JPanel();
+
+        /*set frame properties*/
+        setLayout(new BorderLayout());
+        setTitle("GZipper");
+        setPreferredSize(new Dimension(500, 250));
+        setJMenuBar(_menuBar);
+
+        /*define components*/
+        add(_panel1, BorderLayout.CENTER);
+        add(_panel2, BorderLayout.SOUTH);
+        add(_panel3, BorderLayout.NORTH);
+        _panel1.setLayout(new BorderLayout());
+        _panel2.setLayout(new GridLayout());
+        _panel3.setLayout(new GridLayout());
+
+        _fileMenu.setLabel("File");
+        _exitMenuItem.addActionListener((ActionEvent evt) -> {
+            this.exitMenuActionPerformed(evt);
+        });
+        _helpMenu.setLabel("Help");
+        _aboutMenuItem.addActionListener((ActionEvent evt) -> {
+            this.aboutMenuActionPerformed(evt);
+        });
+
+        _textOutput.setFont(new Font("Consolas", Font.PLAIN, 12));
+        _textOutput.setText("run: \n\n");
+        _textOutput.setEditable(false);
+        _textOutput.setBackground(Color.WHITE);
+
+        _startButton.setEnabled(false);
+        _startButton.addActionListener((ActionEvent evt) -> {
+            this.startButtonActionPerformed(evt);
+        });
+
+        _abortButton.setEnabled(false);
+        _abortButton.addActionListener((ActionEvent evt) -> {
+            this.abortButtonActionPerformed(evt);
+        });
+
+        _selectButton.addActionListener((ActionEvent evt) -> {
+            this.selectButtonActionPerformed(evt);
+        });
+
+        _archiveModeZip.addActionListener((ActionEvent evt) -> {
+            this.archiveModeZipButtonActionPerformed(evt);
+        });
+
+        _archiveModeUnzip.addActionListener((ActionEvent evt) -> {
+            this.archiveModeUnzipButtonActionPerformed(evt);
+        });
+
+        _buttonGroup.add(_archiveModeZip);
+        _buttonGroup.add(_archiveModeUnzip);
+
+        _menuBar.add(_fileMenu);
+        _menuBar.add(_helpMenu);
+        _fileMenu.add(_exitMenuItem);
+        _helpMenu.add(_aboutMenuItem);
+
+        _panel1.add(_textOutput, BorderLayout.CENTER);
+        _panel1.add(_selectButton, BorderLayout.SOUTH);
+        _panel2.add(_startButton);
+        _panel2.add(_abortButton);
+        _panel3.add(_label1);
+        _panel3.add(_archiveModeZip);
+        _panel3.add(_archiveModeUnzip);
+
+        /*add scroll pane to text area*/
+        JScrollPane sp = new JScrollPane(_textOutput,
+                ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED, ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        _panel1.add(sp, BorderLayout.CENTER);
+
+        DefaultCaret caret = (DefaultCaret) _textOutput.getCaret();
+        caret.setUpdatePolicy(DefaultCaret.OUT_BOTTOM); //for auto-scrolling
+
+        pack();
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /*starts compressing/decompressing if start button has been pressed*/
+    private void startButtonActionPerformed(ActionEvent evt) {
+        if (evt.getSource() == _startButton) {
+            _abortButton.setEnabled(true);
+            if (_guiThread == null) {
+                _runFlag = true;
+                _guiThread = new Thread(this);
+                _guiThread.start();
+            }
+            _ps.unpause();
+        }
+    }
+
+    /*aborts the compressing/decompressing operation*/
+    private void abortButtonActionPerformed(ActionEvent evt) {
+        if (evt.getSource() == _abortButton) {
+            _textOutput.append("Trying to abort operation...\n");
+            if (_zipper != null) {
+                _ps.pause();
+                _zipper.interrupt();
+                _zipper.stop();
+                try {
+                    if (_zipper.waitForExecEnd() != false) {
+                        _zipper = null;
+                    }
+                } catch (InterruptedException ex) {
+                    _textOutput.append(ex.toString() + "\n");
+                }
+                _textOutput.append("Operation aborted!\n");
+            }
+        }
+    }
+
+    /*opens a file dialog to either open an archive 
+     or to select (save) files to compress them into an archive*/
+    private void selectButtonActionPerformed(ActionEvent evt) {
+        if (evt.getSource() == _selectButton) {
+            if (_buttonGroup.getSelection() != null) {
+
+                if (_archiveModeZip.isSelected()) {
+                    _fileChooser.setAcceptAllFileFilterUsed(true);
+                    _fileChooser.removeChoosableFileFilter(_filter);
+                    _fileChooser.setMultiSelectionEnabled(true);
+                    _fileChooser.setFileSelectionMode(JFileChooser.FILES_AND_DIRECTORIES);
+                    _fileChooser.showSaveDialog(this);
+                } else { //unzip
+                    _fileChooser.setAcceptAllFileFilterUsed(false);
+                    _fileChooser.setFileFilter(_filter);
+                    _fileChooser.setMultiSelectionEnabled(false);
+                    _fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+                    _fileChooser.showOpenDialog(this);
+                }
+                if (_fileChooser.getSelectedFile() != null) {
+                    /*check file name for illegal characters*/
+                    String filename = _fileChooser.getSelectedFile().getName();
+                    if (filename.contains("<") || filename.contains(">") || filename.contains("/")
+                            || filename.contains("\\") || filename.contains("|") || filename.contains(":")
+                            || filename.contains("*") || filename.contains("\"") || filename.contains("?")) {
+                        drawNewWindow("Warning", "<html><br><p align=\"center\">"
+                                + "&nbsp;Illegal characters found in file name!"
+                                + "<br><br>&nbsp;<u>Characters not allowed:</u> "
+                                + "\\ / | : * \" ? &lt; >&nbsp;<br>&nbsp;</p></html>");
+                    } else {
+                        _textOutput.append("Selection successful; ready to start compression\n");
+                        _startButton.setEnabled(true);
+                    }
+                }
+            } else {
+                drawNewWindow("Warning", "<html><br><p align=\"center\">"
+                        + "&nbsp;You need to select a ZIP-mode first!"
+                        + "&nbsp;</p><br></html>");
+            }
+        }
+    }
+
+    private void archiveModeZipButtonActionPerformed(ActionEvent evt) {
+        if (evt.getSource() == _archiveModeZip) {
+            _selectButton.setText("Select files/folders...");
+        }
+    }
+
+    private void archiveModeUnzipButtonActionPerformed(ActionEvent evt) {
+        if (evt.getSource() == _archiveModeUnzip) {
+            _selectButton.setText("Select archive...");
+        }
+    }
+
+    private void exitMenuActionPerformed(ActionEvent evt) {
+        if (evt.getSource() == _exitMenuItem) {
+            setVisible(false);
+            dispose();
+            System.exit(0);
+        }
+    }
+
+    private void aboutMenuActionPerformed(ActionEvent evt) {
+        if (evt.getSource() == _aboutMenuItem) {
+            drawNewWindow("About", "<html><br><p align=\"center\">&nbsp;2015 (C) "
+                    + "Matthias Fussenegger&nbsp;<br>E-mail: matfu2@me.com</p>"
+                    + "<br>&nbsp;This program uses parts of the commons-compress library by Apache Foundation&nbsp;<br>"
+                    + "&nbsp;and is licensed under the GNU General Public License 3&nbsp;"
+                    + "(<a href=\"http://www.gnu.org/licenses/\">http://www.gnu.org/licenses/</a>)&nbsp;<br>&nbsp;</html>");
+        }
+    }
+
+    /*draw new windows with predefinded title and text*/
+    private void drawNewWindow(String title, String text) {
+        JFrame frame = new JFrame(title);
+        JLabel label = new JLabel(text, SwingConstants.CENTER);
+        JButton button = new JButton("OK");
+        button.addActionListener((ActionEvent evt) -> {
+            if (evt.getActionCommand().equals("OK")) {
+                frame.setVisible(false);
+                frame.dispose();
+            }
+        });
+        label.setFont(new Font("Consolas", Font.BOLD, 12));
+        button.addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyPressed(KeyEvent evt) {
+                if (evt.getKeyCode() == 10) { //10 = return key
+                    button.doClick();
+                }
+            }
+        });
+        frame.setResizable(false);
+        frame.add(label, BorderLayout.CENTER);
+        frame.add(button, BorderLayout.SOUTH);
+        frame.addKeyListener(null);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
+    private void startCompressing() throws InterruptedException {
+        if (_zipper == null) {
+            _zipper = new Zipper(INITIAL_PATH, "gzipper", _fileChooser.getSelectedFiles(), true);
+            try {
+                _zipper.start();
+            } catch (IOException ex) {
+                _textOutput.append(ex.toString() + "\n");
+            } finally {
+                if (_zipper.waitForExecEnd() != false & _zipper != null) {
+                    double seconds = ((double) _zipper.getElapsedTime() / 1E9); //calculate elapsed time in seconds
+                    _textOutput.append("Job done!\n" + "Elapsed time: " + seconds + " seconds\n");
+                }
+            }
+        }
+    }
+
+    private void startDecompressing() throws InterruptedException {
+        if (_zipper == null) {
+            _abortButton.setEnabled(true);
+            if (_isUnix) { //check for OS
+                _zipper = new Zipper(_fileChooser.getSelectedFile().getParent() + "/",
+                        _fileChooser.getSelectedFile().getName(), _fileChooser.getSelectedFiles(), false);
+            } else {
+                _zipper = new Zipper(_fileChooser.getSelectedFile().getParent() + "\\",
+                        _fileChooser.getSelectedFile().getName(), _fileChooser.getSelectedFiles(), false);
+            }
+            try {
+                _zipper.start();
+            } catch (IOException ex) {
+                _textOutput.append(ex.toString() + "\n");
+            } finally {
+                if (_zipper.waitForExecEnd() != false & _zipper != null) {
+                    double seconds = ((double) _zipper.getElapsedTime() / 1E9); //calculate elapsed time in seconds
+                    _textOutput.append("Job done!\n" + "Elapsed time: " + seconds + " seconds\n");
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @author Matthias Fussenegger
+     * @param args
+     * @version 0.6
+     */
+    public static void main(String[] args) {
+        /*String path and decPath used to make directory where JAR-file is located
+         to the current working directory for creating a new archive*/
+        String path = GUI.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        try {
+            String decPath;
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                /*decode path without adding the name of the JAR-file (GZipper.jar = 11 characters)
+                 - for debugging inside an IDE please remove the minus operation (- 11) and make sure
+                 to rename archive name (currently: gzipper) when starting to compress/decompress*/
+                decPath = URLDecoder.decode(path.substring(1, path.length() - 11), "UTF-8");
+            } else {
+                _isUnix = true;
+                decPath = path.substring(0, path.length() - 11);
+            }
+            /*draw application frame*/
+            java.awt.EventQueue.invokeLater(() -> {
+                new GUI(decPath).setVisible(true);
+            });
+            Thread.sleep(300);
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, ex.toString(), ex);
+            System.exit(1);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, ex.toString(), ex);
+        }
+    }
+
+    @Override
+    public void run() {
+        while (_runFlag) {
+            try {
+                _ps.pausePoint();
+                if (_archiveModeZip.isSelected()) {
+                    _textOutput.append("Selected files/folders will be compressed, please wait...\n");
+                    startCompressing();
+                } else { //unzip
+                    _textOutput.append("Selected archive will be decompressed...\n");
+                    startDecompressing();
+                }
+            } catch (InterruptedException ex) {
+                _textOutput.append(ex.toString() + "\n");
+            } finally {
+                _abortButton.setEnabled(false);
+                _zipper = null;
+                _ps.pause();
+            }
+        }
+    }
+}
---
+++ src/gzipper/Zipper.java
@@ -0,0 +1,213 @@
+/*
+ * Copyright (C) 2015 Matthias Fussenegger
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package gzipper;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+import org.apache.commons.compress.archivers.ArchiveEntry;
+import org.apache.commons.compress.archivers.tar.TarArchiveInputStream;
+import org.apache.commons.compress.archivers.tar.TarArchiveOutputStream;
+
+/**
+ *
+ * @author Matthias Fussenegger
+ */
+public class Zipper implements Runnable {
+
+    //ATTRIBUTES
+    private TarArchiveOutputStream _tos;
+    private String _archiveName;
+    private final String _path;
+    private long _elapsedTime;
+    private final boolean _zipMode;
+    private final File[] _selectedFiles;
+
+    private boolean _runFlag;
+    private Thread _zipperThread;
+    private static int _nameIterator; //to avoid overwriting recent archives
+
+    //CONSTRUCTOR
+    public Zipper(String path, String name, File[] files, boolean zipMode) {
+        _path = path;
+        _archiveName = name;
+        _zipMode = zipMode;
+        _selectedFiles = files;
+    }
+
+    //METHODS
+    protected void start() throws IOException {
+        _runFlag = true;
+        _zipperThread = new Thread(this);
+        _zipperThread.start();
+    }
+
+    protected void stop() {
+        _runFlag = false;
+        if (_tos != null) {
+            try {
+                _tos.flush();
+                _tos.close();
+            } catch (IOException ex) {
+                Logger.getLogger(Zipper.class.getName()).log(Level.WARNING, "Archive entry still open", ex);
+                File file = new File(_path + _archiveName + ".tar.gz");
+                if (file.exists()) {
+                    file.delete();
+                }
+            }
+        }
+    }
+
+    protected void interrupt() {
+        _runFlag = false;
+        _zipperThread.interrupt();
+    }
+
+    /*compress files using TAR/GZIP-algorithm and create archive;
+     note that GZIPOutputStream already has a built-in buffer*/
+    private void makeGzip(File[] files, String base) throws IOException {
+        long startTime = System.nanoTime();
+        byte[] buffer = new byte[4096];
+        int readBytes;
+        if (files.length >= 1) {
+            for (int i = 0; i < files.length & _runFlag != false; ++i) {
+                File newFile = files[i];
+                String entryName = base + newFile.getName();
+                if (newFile.isFile()) {
+                    try (BufferedInputStream buf = new BufferedInputStream(new FileInputStream(newFile))) {
+                        ArchiveEntry entry = _tos.createArchiveEntry(newFile, entryName);
+                        _tos.putArchiveEntry(entry);
+                        while ((readBytes = buf.read(buffer)) != -1) {
+                            _tos.write(buffer, 0, readBytes);
+                        }
+                        _tos.closeArchiveEntry();
+                    }
+                } else { //child is a directory
+                    File[] children = getFiles(newFile.getAbsolutePath());
+                    makeGzip(children, entryName + "/"); //the slash indicates a folder, see JavaDoc
+                }
+            }
+        }
+        _elapsedTime = System.nanoTime() - startTime;
+    }
+
+    /*extract a TAR/GZIP-archive; note that GZIPInputStream already has a built-in buffer*/
+    private void extractGzip(String path, String name) throws IOException {
+        long startTime = System.nanoTime();
+        try (TarArchiveInputStream tis = new TarArchiveInputStream(new GZIPInputStream(new FileInputStream(path + name)))) {
+            ArchiveEntry entry = tis.getNextEntry();
+            /*create main folder of gzip archive*/
+            File folder = new File(path + name.substring(0, 7));
+            if (!folder.exists()) {
+                folder.mkdir();
+            }
+            while (entry != null & _runFlag != false) {
+                String entryName = entry.getName();
+                /*check if entry contains a directory*/
+                if (entryName.contains("/")) {
+                    File newFile;
+                    if (GUI._isUnix) {//check OS for correct file path
+                        newFile = new File(folder.getAbsolutePath() + "/" + entryName);
+                    } else {
+                        newFile = new File(folder.getAbsolutePath() + "\\" + entryName);
+                    }
+                    if (!newFile.getParentFile().exists()) {
+                        newFile.getParentFile().mkdirs();
+                    }
+                }
+                String newFilePath;
+                if (GUI._isUnix) { //check OS for correct file path
+                    newFilePath = folder.getAbsolutePath() + "/" + entryName;
+                } else {
+                    newFilePath = folder.getAbsolutePath() + "\\" + entryName;
+                }
+                /*create new OutputStream and write bytes to new file*/
+                try (BufferedOutputStream buf = new BufferedOutputStream(new FileOutputStream(newFilePath))) {
+                    byte[] buffer = new byte[4096];
+                    int readBytes;
+                    while ((readBytes = tis.read(buffer)) != -1) {
+                        buf.write(buffer, 0, readBytes);
+                    }
+                }
+                entry = tis.getNextEntry();
+            }
+        }
+        _elapsedTime = System.nanoTime() - startTime;
+    }
+
+    /*retrieve files from a specific directory; mandatory for compression*/
+    private File[] getFiles(String path) throws IOException {
+        File dir = new File(path);
+        File[] files = dir.listFiles();
+        return files;
+    }
+
+    /*called by other classes to wait for thread to die*/
+    protected boolean waitForExecEnd() throws InterruptedException {
+        if (_zipperThread != null) {
+            _zipperThread.join();
+            return true;
+        }
+        return false;
+    }
+
+    protected long getElapsedTime() {
+        return _elapsedTime;
+    }
+
+    @Override
+    public void run() {
+        /*in current implamantation multiple jobs are not yet supported*/
+        if (_zipMode != false) {
+            try {
+                /*check whether archive with given name already exists;
+                 if so, add iterator to file name an re-check*/
+                File file = new File(_path + _archiveName + ".tar.gz");
+                while (file.exists()) {
+                    ++_nameIterator;
+                    _archiveName = _archiveName.substring(0, 7) + _nameIterator;
+                    file = new File(_path + _archiveName + ".tar.gz");
+                }
+                _tos = new TarArchiveOutputStream(new GZIPOutputStream(new FileOutputStream(_path + _archiveName + ".tar.gz")));
+                _tos.setLongFileMode(TarArchiveOutputStream.LONGFILE_GNU);
+            } catch (IOException ex) {
+                Logger.getLogger(Zipper.class.getName()).log(Level.SEVERE, "Error creating output stream", ex);
+                System.exit(1);
+            }
+        }
+        while (_runFlag) {
+            try {
+                if (_zipMode != false & _selectedFiles != null) {
+                    makeGzip(_selectedFiles, "");
+                } else { //unzip
+                    extractGzip(_path, _archiveName);
+                }
+                stop();
+            } catch (IOException ex) {
+                Logger.getLogger(Zipper.class.getName()).log(Level.SEVERE, "Error compressing archive", ex);
+                System.exit(1);
+            }
+        }
+    }
+}
