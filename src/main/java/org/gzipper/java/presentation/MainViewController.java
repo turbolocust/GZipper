61f9672738f7283b8759c638321a6b11dfc4d870
Matthias Fussenegger <matfu2@gmail.com>
2017-03-13 19:37:56 +0200

    Updates to Controller members

--- src/main/java/org/gzipper/java/presentation/MainViewController.java
+++ src/main/java/org/gzipper/java/presentation/MainViewController.java
@@ -33,11 +33,14 @@ import org.gzipper.java.application.util.Settings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleGroup;
 import javafx.scene.image.Image;
 
 /**
@@ -46,72 +49,133 @@ import javafx.scene.image.Image;
  */
 public class MainViewController extends BaseController implements Initializable {
 
-	@FXML
-	private MenuItem _closeMenuItem;
+    /**
+     * Key constant used to access the properties map for menu items.
+     */
+    private static final String COMPRESSION_LEVEL_KEY = "compressionLevel";
 
-	@FXML
-	private MenuItem _deleteMenuItem;
+    /**
+     * The quality of compression.
+     */
+    private int _compressionLevel;
 
-	@FXML
-	private TextArea _textArea;
+    @FXML
+    private Button _selectFilesButton;
 
-	@FXML
-	private TextField _outputPath;
+    @FXML
+    private Button _startButton;
 
-	@FXML
-	private ComboBox<String> _archiveTypeComboBox;
+    @FXML
+    private Button _abortButton;
 
-	@FXML
-	private void handleCloseMenuItemAction(ActionEvent evt) {
-		if (evt.getSource().equals(_closeMenuItem)) {
-			_primaryStage.close();
-			System.exit(0);
-		}
-	}
+    @FXML
+    private TextArea _textArea;
 
-	@FXML
-	private void handleDeleteMenuItemAction(ActionEvent evt) {
-		if (evt.getSource().equals(_deleteMenuItem)) {
-			Optional<ButtonType> result = AlertDialog.showConfirmationDialog(
-					_resources.getString("clearTextWarning.text"), _resources.getString("confirmation.text"));
-			if (result.isPresent() && result.get() == ButtonType.YES) {
-				_textArea.clear();
-				_textArea.setText("run:\n");
-			}
-		}
-	}
+    @FXML
+    private MenuItem _veryLowCompressionMenuItem;
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
+    @FXML
+    private MenuItem _lowCompressionMenuItem;
 
-		try {
+    @FXML
+    private MenuItem _mediumCompressionMenuItem;
 
-			final String decPath = AppUtil.getDecodedRootPath(GZipper.class);
-			OperatingSystem os; // to determine the users operating system
+    @FXML
+    private MenuItem _goodCompressionMenuItem;
 
-			String settingsFile;
-			try { // locate settings file
-				settingsFile = AppUtil.getResource(GZipper.class, "/settings.properties");
-			} catch (URISyntaxException ex) {
-				Logger.getLogger(GZipper.class.getName()).log(Level.SEVERE, null, ex);
-				settingsFile = decPath + "settings.properties";
-			}
+    @FXML
+    private MenuItem _bestCompressionMenuItem;
 
-			if (System.getProperty("os.name").startsWith("Windows")) {
-				os = new Windows();
-			} else {
-				os = new Unix();
-			}
+    @FXML
+    private MenuItem _closeMenuItem;
 
-			_settings = new Settings(settingsFile, os);
+    @FXML
+    private MenuItem _deleteMenuItem;
 
-		} catch (IOException ex) {
-			Logger.getLogger(GZipper.class.getName()).log(Level.SEVERE, null, ex);
-		}
+    @FXML
+    private MenuItem _aboutMenuItem;
 
-		_resources = resources;
-		_frameImage = new Image("/images/icon_32.png");
+    @FXML
+    private CheckMenuItem _enableLoggingCheckMenuItem;
 
-		_textArea.setText("run:\n" + _resources.getString("changeOutputPath.text") + "\n");
-	}
+    @FXML
+    private ToggleGroup _compressionType;
+
+    @FXML
+    private TextField _outputPath;
+
+    @FXML
+    private ComboBox<String> _archiveTypeComboBox;
+
+    @FXML
+    private void handleCompressionLevelMenuItemAction(ActionEvent evt) {
+        final MenuItem selectedItem = (MenuItem) evt.getSource();
+        Object compressionLevel = selectedItem.getProperties().get("compressionLevel");
+
+        if (compressionLevel != null) {
+            _compressionLevel = (int) compressionLevel;
+            Logger.getLogger(GZipper.class.getName()).log(Level.INFO,
+                    "Compression level set to: {0}", _compressionLevel);
+        }
+    }
+
+    @FXML
+    private void handleCloseMenuItemAction(ActionEvent evt) {
+        if (evt.getSource().equals(_closeMenuItem)) {
+            _primaryStage.close();
+            System.exit(0);
+        }
+    }
+
+    @FXML
+    private void handleDeleteMenuItemAction(ActionEvent evt) {
+        if (evt.getSource().equals(_deleteMenuItem)) {
+            Optional<ButtonType> result = AlertDialog.showConfirmationDialog(
+                    _resources.getString("clearTextWarning.text"), _resources.getString("confirmation.text"));
+            if (result.isPresent() && result.get() == ButtonType.YES) {
+                _textArea.clear();
+                _textArea.setText("run:\n");
+            }
+        }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        try {
+
+            final String decPath = AppUtil.getDecodedRootPath(GZipper.class);
+            OperatingSystem os; // to determine the users operating system
+
+            String settingsFile;
+            try { // locate settings file
+                settingsFile = AppUtil.getResource(GZipper.class, "/settings.properties");
+            } catch (URISyntaxException ex) {
+                Logger.getLogger(GZipper.class.getName()).log(Level.SEVERE, null, ex);
+                settingsFile = decPath + "settings.properties";
+            }
+
+            if (System.getProperty("os.name").startsWith("Windows")) {
+                os = new Windows();
+            } else {
+                os = new Unix();
+            }
+
+            _settings = new Settings(settingsFile, os);
+
+            _veryLowCompressionMenuItem.getProperties().put(COMPRESSION_LEVEL_KEY, 0);
+            _lowCompressionMenuItem.getProperties().put(COMPRESSION_LEVEL_KEY, 0);
+            _mediumCompressionMenuItem.getProperties().put(COMPRESSION_LEVEL_KEY, 0);
+            _goodCompressionMenuItem.getProperties().put(COMPRESSION_LEVEL_KEY, 0);
+            _bestCompressionMenuItem.getProperties().put(COMPRESSION_LEVEL_KEY, 0);
+
+        } catch (IOException ex) {
+            Logger.getLogger(GZipper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+        _resources = resources;
+        _frameImage = new Image("/images/icon_32.png");
+
+        _textArea.setText("run:\n" + _resources.getString("changeOutputPath.text") + "\n");
+    }
 }
