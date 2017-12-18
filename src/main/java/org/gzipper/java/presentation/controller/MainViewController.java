bfcb67e346511f2989daa0742db0541a1c5984eb
Matthias Fussenegger <matfu2@gmail.com>
2017-12-18 18:06:46 +0100

    ConcurrentMap for managing tasks and preparation for next release

--- src/main/java/org/gzipper/java/presentation/controller/MainViewController.java
+++ src/main/java/org/gzipper/java/presentation/controller/MainViewController.java
@@ -20,12 +20,12 @@ import java.io.File;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 import java.util.ResourceBundle;
 import java.util.concurrent.CancellationException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Future;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -96,7 +96,7 @@ public class MainViewController extends BaseController {
      * Map consisting of {@link Future} objects representing the currently
      * active tasks.
      */
-    private final Map<String, Future<?>> _activeTasks;
+    private final ConcurrentMap<String, Future<?>> _activeTasks;
 
     /**
      * The currently active state.
@@ -128,70 +128,70 @@ public class MainViewController extends BaseController {
      * The compression level. Initialized with default compression level.
      */
     private int _compressionLevel;
-    
+
     @FXML
     private MenuItem _noCompressionMenuItem;
-    
+
     @FXML
     private MenuItem _bestSpeedCompressionMenuItem;
-    
+
     @FXML
     private MenuItem _defaultCompressionMenuItem;
-    
+
     @FXML
     private MenuItem _bestCompressionMenuItem;
-    
+
     @FXML
     private MenuItem _closeMenuItem;
-    
+
     @FXML
     private MenuItem _deleteMenuItem;
-    
+
     @FXML
     private MenuItem _dropAddressesMenuItem;
-    
+
     @FXML
     private MenuItem _resetAppMenuItem;
-    
+
     @FXML
     private MenuItem _aboutMenuItem;
-    
+
     @FXML
     private RadioButton _compressRadioButton;
-    
+
     @FXML
     private RadioButton _decompressRadioButton;
-    
+
     @FXML
     private TextArea _textArea;
-    
+
     @FXML
     private CheckMenuItem _enableLoggingCheckMenuItem;
-    
+
     @FXML
     private CheckMenuItem _enableDarkThemeCheckMenuItem;
-    
+
     @FXML
     private TextField _outputPathTextField;
-    
+
     @FXML
     private ComboBox<ArchiveType> _archiveTypeComboBox;
-    
+
     @FXML
     private Button _startButton;
-    
+
     @FXML
     private Button _abortButton;
-    
+
     @FXML
     private Button _selectFilesButton;
-    
+
     @FXML
     private Button _saveAsButton;
-    
+
     @FXML
     private ProgressBar _progressBar;
-    
+
     @FXML
     private Text _progressText;
 
@@ -206,7 +206,7 @@ public class MainViewController extends BaseController {
         super(theme, hostServices);
         _archiveName = DEFAULT_ARCHIVE_NAME;
         _compressionLevel = Deflater.DEFAULT_COMPRESSION;
-        _activeTasks = Collections.synchronizedMap(new HashMap<>());
+        _activeTasks = new ConcurrentHashMap<>();
         Log.i("Default archive name set to: {0}", _archiveName, false);
     }
 
@@ -222,7 +222,7 @@ public class MainViewController extends BaseController {
             });
         }
     }
-    
+
     @FXML
     void handleCompressionLevelMenuItemAction(ActionEvent evt) {
         final MenuItem selectedItem = (MenuItem) evt.getSource();
@@ -233,7 +233,7 @@ public class MainViewController extends BaseController {
                     selectedItem.getText());
         }
     }
-    
+
     @FXML
     void handleCloseMenuItemAction(ActionEvent evt) {
         if (evt.getSource().equals(_closeMenuItem)) {
@@ -241,7 +241,7 @@ public class MainViewController extends BaseController {
             exit();
         }
     }
-    
+
     @FXML
     void handleDeleteMenuItemAction(ActionEvent evt) {
         if (evt.getSource().equals(_deleteMenuItem)) {
@@ -255,7 +255,7 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     @FXML
     void handleDropAddressesMenuItemAction(ActionEvent evt) {
         if (evt.getSource().equals(_dropAddressesMenuItem)) {
@@ -275,7 +275,7 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     @FXML
     void handleResetAppMenuItemAction(ActionEvent evt) {
         if (evt.getSource().equals(_resetAppMenuItem)) {
@@ -288,14 +288,14 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     @FXML
     void handleAboutMenuItemAction(ActionEvent evt) {
         if (evt.getSource().equals(_aboutMenuItem)) {
             ViewControllers.showAboutView(_theme, _hostServices);
         }
     }
-    
+
     @FXML
     void handleStartButtonAction(ActionEvent evt) {
         if (evt.getSource().equals(_startButton)) {
@@ -324,18 +324,18 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     @FXML
     void handleAbortButtonAction(ActionEvent evt) {
         if (evt.getSource().equals(_abortButton)) {
             cancelActiveTasks();
         }
     }
-    
+
     @FXML
     void handleSelectFilesButtonAction(ActionEvent evt) {
         if (evt.getSource().equals(_selectFilesButton)) {
-            
+
             FileChooser fc = new FileChooser();
             if (_compressRadioButton.isSelected()) {
                 fc.setTitle(I18N.getString("browseForFiles.text"));
@@ -343,9 +343,9 @@ public class MainViewController extends BaseController {
                 fc.setTitle(I18N.getString("browseForArchive.text"));
                 _state.applyExtensionFilters(fc);
             }
-            
+
             List<File> selectedFiles = fc.showOpenMultipleDialog(_primaryStage);
-            
+
             String message;
             if (selectedFiles != null) {
                 _startButton.setDisable(false);
@@ -365,11 +365,11 @@ public class MainViewController extends BaseController {
             Log.i(message, true);
         }
     }
-    
+
     @FXML
     void handleSaveAsButtonAction(ActionEvent evt) {
         if (evt.getSource().equals(_saveAsButton)) {
-            
+
             final File file;
             if (_compressRadioButton.isSelected()) {
                 FileChooser fc = new FileChooser();
@@ -381,7 +381,7 @@ public class MainViewController extends BaseController {
                 dc.setTitle(I18N.getString("saveAsPathTitle.text"));
                 file = dc.showDialog(_primaryStage);
             }
-            
+
             if (file != null) {
                 updateSelectedFile(file);
                 String absolutePath = file.getAbsolutePath();
@@ -393,7 +393,7 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     @FXML
     void handleModeRadioButtonAction(ActionEvent evt) {
         if (evt.getSource().equals(_compressRadioButton)) {
@@ -403,7 +403,7 @@ public class MainViewController extends BaseController {
         }
         resetSelections();
     }
-    
+
     @FXML
     void handleArchiveTypeComboBoxAction(ActionEvent evt) {
         if (evt.getSource().equals(_archiveTypeComboBox)) {
@@ -430,7 +430,7 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     @FXML
     void onOutputPathTextFieldKeyTyped(KeyEvent evt) {
         if (evt.getSource().equals(_outputPathTextField)) {
@@ -440,7 +440,7 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     @FXML
     void handleEnableLoggingCheckMenuItemAction(ActionEvent evt) {
         if (evt.getSource().equals(_enableLoggingCheckMenuItem)) {
@@ -449,7 +449,7 @@ public class MainViewController extends BaseController {
             Log.setVerboseUiLogging(enableLogging);
         }
     }
-    
+
     @FXML
     void handleEnableDarkThemeCheckMenuItemAction(ActionEvent evt) {
         if (evt.getSource().equals(_enableDarkThemeCheckMenuItem)) {
@@ -556,9 +556,9 @@ public class MainViewController extends BaseController {
         Task<Boolean> task = new Task<Boolean>() {
             @Override
             protected Boolean call() throws Exception {
-                
+
                 Future<Boolean> futureTask = TaskHandler.submit(operation);
-                
+
                 while (!futureTask.isDone()) {
                     try {
                         Thread.sleep(10); // check for interruption
@@ -631,12 +631,12 @@ public class MainViewController extends BaseController {
         logger.setUseParentHandlers(false);
         logger.addHandler(handler);
     }
-    
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         Settings settings = Settings.getInstance();
         OperatingSystem os = settings.getOs();
-        
+
         initLogger();
 
         // set recently used path from settings if valid
@@ -751,7 +751,7 @@ public class MainViewController extends BaseController {
                 toggleUIcontrols(true);
             }
         }
-        
+
         @Override
         public void update(Notifier<Integer> notifier, Integer value) {
             if (value >= 100) {
@@ -772,15 +772,15 @@ public class MainViewController extends BaseController {
             }
         }
     }
-    
+
     private class CompressState extends ArchivingState {
-        
+
         @Override
         public boolean validateOutputPath() {
-            
+
             String outputPath = _outputPathTextField.getText();
             String extName = _archiveTypeComboBox.getValue().getDefaultExtensionName(false);
-            
+
             if (FileUtils.isValidDirectory(outputPath)) {
                 // user has not specified output filename
                 outputPath = FileUtils.generateUniqueFilename(outputPath,
@@ -793,7 +793,7 @@ public class MainViewController extends BaseController {
             }
             return false;
         }
-        
+
         @Override
         public void performOperation(ArchiveOperation operation) {
             if (!ListUtils.isNullOrEmpty(_selectedFiles)) {
@@ -803,7 +803,7 @@ public class MainViewController extends BaseController {
                 Log.i(I18N.getString("noFilesSelectedWarning.text"), true);
             }
         }
-        
+
         @Override
         public List<ArchiveOperation> initOperation(ArchiveType archiveType)
                 throws GZipperException {
@@ -827,14 +827,14 @@ public class MainViewController extends BaseController {
             return operations;
         }
     }
-    
+
     private class DecompressState extends ArchivingState {
-        
+
         @Override
         public boolean validateOutputPath() {
             return FileUtils.isValidDirectory(_outputPathTextField.getText());
         }
-        
+
         @Override
         public void performOperation(ArchiveOperation operation) {
             if (_outputFile != null && !ListUtils.isNullOrEmpty(_selectedFiles)) {
@@ -845,7 +845,7 @@ public class MainViewController extends BaseController {
                 _outputPathTextField.requestFocus();
             }
         }
-        
+
         @Override
         public List<ArchiveOperation> initOperation(ArchiveType archiveType)
                 throws GZipperException {
