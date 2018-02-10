188dbac41130355d2353020d0f88f4a966b10058
Matthias Fussenegger <matfu2@gmail.com>
2018-02-10 17:15:30 +0100

    Add Predicate.java and PatternPredicate.java

---
+++ src/main/java/org/gzipper/java/application/predicates/PatternPredicate.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (C) 2018 Matthias Fussenegger
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gzipper.java.application.predicates;
+
+import java.util.function.Predicate;
+import java.util.regex.Pattern;
+
+/**
+ * Simple filter predicate that tests a string against a {@link Pattern}.
+ *
+ * @author Matthias Fussenegger
+ */
+public class PatternPredicate implements Predicate<String> {
+
+    /**
+     * The aggregated pattern (regular expression).
+     */
+    private final Pattern _pattern;
+
+    public PatternPredicate(Pattern pattern) {
+        _pattern = pattern;
+    }
+
+    @Override
+    public boolean test(String t) {
+        return _pattern.matcher(t).matches();
+    }
+}
