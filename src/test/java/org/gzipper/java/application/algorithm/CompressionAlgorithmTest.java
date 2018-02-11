b78ca033394cee8ded20e292aac27c9646ca1303
Matthias Fussenegger <matfu2@gmail.com>
2018-02-11 17:18:06 +0100

    Implemented unit tests

---
+++ src/test/java/org/gzipper/java/application/algorithm/CompressionAlgorithmTest.java
@@ -0,0 +1,214 @@
+/*
+ * Copyright (C) 2018 Matthias Fussenegger
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gzipper.java.application.algorithm;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Iterator;
+import org.gzipper.java.application.algorithm.TestUtils.TestObject;
+import org.gzipper.java.application.algorithm.type.*;
+import org.gzipper.java.application.model.OperatingSystem;
+import org.gzipper.java.application.util.FileUtils;
+import org.gzipper.java.util.Settings;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Test of {@link CompressionAlgorithm} interface and its realizations.
+ *
+ * @author Matthias Fussenegger
+ */
+public class CompressionAlgorithmTest {
+
+    private String _archiveFileNamePrefix;
+
+    private String _testFileNamePrefix;
+
+    private final String _tempDirectory;
+
+    public CompressionAlgorithmTest() throws IOException {
+        File temp = File.createTempFile("gzipper_temp_file", null);
+        _tempDirectory = temp.getAbsoluteFile().getParent();
+    }
+
+    @BeforeClass
+    public static void setUpClass() {
+        OperatingSystem os = TestUtils.getOperatingSystem();
+        Settings.getInstance().init(null, os);
+    }
+
+    @Before
+    public void setUp() {
+        final LocalTime time = LocalTime.now();
+        // define name of test archive
+        _archiveFileNamePrefix = "gzipper_aa_test_archive-"
+                + LocalDate.now() + "-" + time.getNano();
+        _archiveFileNamePrefix = _archiveFileNamePrefix.replaceAll(":", "_");
+        assertFalse(FileUtils.containsIllegalChars(_archiveFileNamePrefix));
+        // define name of test file (to be archived)
+        _testFileNamePrefix = "gzipper_aa_test_file-"
+                + LocalDate.now() + "-" + time.getNano();
+        _testFileNamePrefix = _testFileNamePrefix.replaceAll(":", "_");
+        assertFalse(FileUtils.containsIllegalChars(_testFileNamePrefix));
+    }
+
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of compress and extract method, of class ArchivingAlgorithm.
+     */
+    @Test
+    public void testZipCompressExtract() {
+        try {
+            System.out.println("ZIP test");
+            testCompressionExtraction(new Zip(), ".zip");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testJarCompressExtract() {
+        try {
+            System.out.println("JAR test");
+            testCompressionExtraction(new Jar(), ".jar");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testGzipCompressExtract() {
+        try {
+            System.out.println("GZIP test");
+            testCompressionExtraction(new Gzip(), ".gzip");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testTarGzCompressExtract() {
+        try {
+            System.out.println("TAR+GZ test");
+            testCompressionExtraction(new Tarball(), ".tgz");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testTarBzip2CompressExtract() {
+        try {
+            System.out.println("TAR+BZIP2 test");
+            testCompressionExtraction(new TarBzip2(), ".tbz2");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+    }
+
+    @Test
+    public void testTarLzmaCompressExtract() {
+        try {
+            System.out.println("TAR+LZMA test");
+            testCompressionExtraction(new TarLzma(), ".tlz");
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+    }
+
+    private void testCompressionExtraction(
+            CompressionAlgorithm instance, String suffix) throws Exception {
+
+        final String location = _tempDirectory;
+        final String name = _archiveFileNamePrefix + suffix;
+        final String fullname = FileUtils
+                .combinePathAndFilename(location, name);
+
+        final TestObject testObj = TestUtils
+                .generateTestObject(_tempDirectory, _testFileNamePrefix);
+
+        final File testFile = testObj._testFile;
+        final File archiveFile = new File(fullname);
+
+        try {
+            System.out.println("compress");
+
+            final File[] files = {testFile};
+            instance.compress(files, location, name);
+
+            // build output file location
+            final StringBuilder sb = new StringBuilder(location);
+            if (!location.endsWith(File.separator)) {
+                sb.append(File.separator);
+            }
+
+            System.out.println("extract");
+            instance.extract(sb.toString(), fullname); // overwrite existing
+
+            // build output file name
+            File outputFolder = null;
+            if (!(instance instanceof Gzip)) { // gzip does not create output folder
+                sb.append(_archiveFileNamePrefix);
+                outputFolder = new File(sb.toString());
+                assertTrue(outputFolder.exists());
+                sb.append(File.separator);
+            }
+
+            sb.append(testFile.getName());
+            final File extractedFile = new File(sb.toString());
+            assertTrue(extractedFile.exists());
+
+            try {
+                // validate extracted file
+                final Iterator<String> iter = testObj._fileContent.linesIterator();
+                try (FileReader fr = new FileReader(extractedFile);
+                        BufferedReader reader = new BufferedReader(fr)) {
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        assertTrue(iter.hasNext());
+                        assertEquals(line, iter.next());
+                    }
+                }
+                System.out.println("Test successful.");
+            } finally {
+                extractedFile.delete();
+                if (outputFolder != null) {
+                    outputFolder.delete();
+                }
+            }
+        } finally {
+            testFile.delete();
+            archiveFile.delete();
+        }
+    }
+}
---
+++ src/test/java/org/gzipper/java/application/algorithm/TestUtils.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright (C) 2018 Matthias Fussenegger
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.gzipper.java.application.algorithm;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+import org.gzipper.java.application.model.OperatingSystem;
+import org.gzipper.java.application.model.Unix;
+import org.gzipper.java.application.model.Windows;
+import org.gzipper.java.application.util.FileUtils;
+
+/**
+ *
+ * @author Matthias Fussenegger
+ */
+public final class TestUtils {
+
+    private static final int NUMBER_OF_LINES = 32;
+
+    private static final int LINE_LENGTH = 256;
+
+    static String generateRandomString(int length) {
+        final StringBuilder sb = new StringBuilder(length);
+        final Random rand = new Random();
+        for (int i = 0; i < length; ++i) {
+            char c = (char) (rand.nextInt(126 - 32) + 32);
+            sb.append(Character.toString(c));
+        }
+        return sb.toString();
+    }
+
+    static TestObject generateTestObject(String dir, String fname) throws IOException {
+
+        String filename = FileUtils.combinePathAndFilename(dir, fname);
+        File file = File.createTempFile(filename, null);
+
+        Document document = new Document();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
+            for (int i = 0; i < NUMBER_OF_LINES; ++i) {
+                String line = TestUtils.generateRandomString(LINE_LENGTH);
+                writer.append(line);
+                document.appendLine(line);
+                writer.newLine();
+                writer.flush();
+            }
+        }
+
+        return new TestObject(file, document);
+    }
+
+    static OperatingSystem getOperatingSystem() {
+        return System.getProperty("os.name").startsWith("Windows")
+                ? new Windows()
+                : new Unix();
+    }
+
+    static class Document {
+
+        final List<String> _lines;
+
+        private Document() {
+            _lines = new ArrayList<>();
+        }
+
+        void appendLine(String line) {
+            _lines.add(line);
+        }
+
+        Iterator<String> linesIterator() {
+            return _lines.iterator();
+        }
+    }
+
+    static class TestObject {
+
+        final File _testFile;
+
+        final Document _fileContent;
+
+        private TestObject(File testFile, Document fileContent) {
+            _testFile = testFile;
+            _fileContent = fileContent;
+        }
+    }
+}
