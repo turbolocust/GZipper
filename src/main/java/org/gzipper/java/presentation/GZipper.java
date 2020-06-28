eada6105f945fda01f6bd5498912601b22478cf4
Matthias Fussenegger <matfu2@gmail.com>
2020-06-28 14:38:54 +0200

    Support for launch arguments to directly launch to HashView

--- src/main/java/org/gzipper/java/presentation/GZipper.java
+++ src/main/java/org/gzipper/java/presentation/GZipper.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 Matthias Fussenegger
+ * Copyright (C) 2020 Matthias Fussenegger
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,8 +20,12 @@ import java.io.File;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URISyntaxException;
+
+import javafx.scene.Parent;
 import org.gzipper.java.presentation.controller.BaseController;
+import org.gzipper.java.presentation.controller.HashViewController;
 import org.gzipper.java.presentation.controller.MainViewController;
+
 import java.util.ResourceBundle;
 import java.util.logging.FileHandler;
 import java.util.logging.Handler;
@@ -43,10 +47,133 @@ import org.gzipper.java.util.Settings;
 import org.gzipper.java.util.Log;
 
 /**
- *
  * @author Matthias Fussenegger
  */
-public class GZipper extends Application {
+public final class GZipper extends Application {
+
+    //<editor-fold desc="Constants">
+
+    private static final String LAUNCH_MODE_PARAM_NAME = "launch_mode";
+    private static final String LAUNCH_MODE_APPLICATION = "application";
+    private static final String LAUNCH_MODE_HASH_VIEW = "hashview";
+
+    //</editor-fold>
+
+    //<editor-fold desc="Initialization">
+
+    private void initApplication() {
+        try {
+            final String decPath = AppUtils.getDecodedRootPath(getClass());
+
+            File settings = new File(decPath + "settings.properties");
+            if (!settings.exists()) {
+                try { // copy settings file to application folder if missing
+                    String resource = AppUtils.getResource(GZipper.class, "/settings.properties");
+                    FileUtils.copy(resource, decPath + "settings.properties");
+                } catch (URISyntaxException | IOException ex) {
+                    Log.e(ex.getLocalizedMessage(), ex);
+                }
+            }
+
+            // determine operating system and initialize settings class
+            OperatingSystem os = System.getProperty("os.name")
+                    .toLowerCase().startsWith("windows")
+                    ? new OperatingSystem(OS.WINDOWS)
+                    : new OperatingSystem(OS.UNIX);
+
+            Settings.getInstance().init(settings, os);
+
+        } catch (UnsupportedEncodingException ex) {
+            Log.e(ex.getLocalizedMessage(), ex);
+        }
+    }
+
+    private void initLogger() {
+
+        Logger logger = Log.DEFAULT_LOGGER;
+
+        try {
+            final String decPath = AppUtils.getDecodedRootPath(getClass());
+            FileHandler handler = new FileHandler(decPath + "gzipper.log");
+            handler.setFormatter(new SimpleFormatter());
+            logger.addHandler(handler);
+            Log.setVerboseUiLogging(true);
+        } catch (IOException | SecurityException ex) {
+            logger.log(Level.SEVERE, null, ex);
+        }
+    }
+
+    //</editor-fold>
+
+    //<editor-fold desc="Loading of views">
+
+    private Parent loadMainView(Stage stage, CSS.Theme theme) throws IOException {
+        var fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
+        var controller = new MainViewController(theme, getHostServices());
+        controller.setPrimaryStage(stage);
+
+        fxmlLoader.setResources(getDefaultResourceBundle());
+        fxmlLoader.setController(controller);
+
+        // properly shut down application when closing/hiding window
+        stage.setOnCloseRequest((WindowEvent evt) -> {
+            evt.consume();
+            controller.cancelActiveTasks();
+            exitApplication();
+        });
+
+        stage.setOnHiding((WindowEvent evt) -> {
+            evt.consume();
+            controller.cancelActiveTasks();
+            exitApplication();
+        });
+
+        return fxmlLoader.load();
+    }
+
+    private Parent loadHashView(Stage stage, CSS.Theme theme) throws IOException {
+        var fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/HashView.fxml"));
+        var controller = new HashViewController(theme);
+        controller.setPrimaryStage(stage);
+
+        fxmlLoader.setResources(getDefaultResourceBundle());
+        fxmlLoader.setController(controller);
+
+        // properly shut down application when closing/hiding window
+        stage.setOnCloseRequest((WindowEvent evt) -> {
+            evt.consume();
+            controller.interrupt();
+            exitApplication();
+        });
+
+        stage.setOnHiding((WindowEvent evt) -> {
+            evt.consume();
+            controller.interrupt();
+            exitApplication();
+        });
+
+        return fxmlLoader.load();
+    }
+
+    //</editor-fold>
+
+    /**
+     * Returns the default resource bundle to be used by this application.
+     *
+     * @return the default resource bundle to be used by this application.
+     */
+    private ResourceBundle getDefaultResourceBundle() {
+        return ResourceBundle.getBundle("i18n/gzipperMainView");
+    }
+
+    /**
+     * Exits the application gracefully.
+     */
+    private void exitApplication() {
+        Log.i("Exiting application.", false);
+        Platform.exit();
+        System.exit(0);
+    }
 
     @Override
     public final void start(Stage stage) throws Exception {
@@ -63,97 +190,36 @@ public class GZipper extends Application {
         }
 
         // set correct theme based on settings
-        final CSS.Theme theme = enableDarkTheme
-                ? CSS.Theme.DARK_THEME : CSS.Theme.MODENA;
+        final CSS.Theme theme = enableDarkTheme ? CSS.Theme.DARK_THEME : CSS.Theme.MODENA;
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/MainView.fxml"));
-        MainViewController controller = new MainViewController(theme, getHostServices());
+        var params = getParameters().getNamed(); // is never null
+        String launchMode = params.get(LAUNCH_MODE_PARAM_NAME);
+        if (launchMode == null) launchMode = LAUNCH_MODE_APPLICATION;
 
-        fxmlLoader.setResources(ResourceBundle.getBundle("i18n/gzipperMainView"));
-        fxmlLoader.setController(controller);
+        Parent parent;
+        switch (launchMode) {
+            case LAUNCH_MODE_APPLICATION:
+                parent = loadMainView(stage, theme);
+                break;
+            case LAUNCH_MODE_HASH_VIEW:
+                parent = loadHashView(stage, theme);
+                break;
+            default:
+                throw new IllegalArgumentException(String.format(
+                        "Value of parameter '%s' is unknown", LAUNCH_MODE_PARAM_NAME));
+        }
 
         // load parent to initialize scene
-        Scene scene = new Scene(fxmlLoader.load());
+        Scene scene = new Scene(parent);
         BaseController.getStages().add(stage);
-        controller.setPrimaryStage(stage);
 
         // load CSS theme
         CSS.load(theme, scene);
-
-        // properly shut down application when closing/hiding window
-        stage.setOnCloseRequest((WindowEvent evt) -> {
-            evt.consume();
-            controller.cancelActiveTasks();
-            exitApplication();
-        });
-        stage.setOnHiding((WindowEvent evt) -> {
-            evt.consume();
-            controller.cancelActiveTasks();
-            exitApplication();
-        });
 
         stage.setTitle("GZipper");
         stage.getIcons().add(BaseController.getIconImage());
         stage.setScene(scene);
         stage.show();
-    }
-
-    /**
-     * Initializes the application.
-     */
-    private void initApplication() {
-        try {
-            final String decPath = AppUtils.getDecodedRootPath(getClass());
-
-            File settings = new File(decPath + "settings.properties");
-            if (!settings.exists()) {
-                try { // copy settings file to application folder if missing
-                    String resource = AppUtils.getResource(GZipper.class, "/settings.properties");
-                    FileUtils.copy(resource, decPath + "settings.properties");
-                }
-                catch (URISyntaxException | IOException ex) {
-                    Log.e(ex.getLocalizedMessage(), ex);
-                }
-            }
-
-            // determine operating system and initialize settings class
-            OperatingSystem os = System.getProperty("os.name")
-                    .toLowerCase().startsWith("windows")
-                    ? new OperatingSystem(OS.WINDOWS)
-                    : new OperatingSystem(OS.UNIX);
-
-            Settings.getInstance().init(settings, os);
-        }
-        catch (UnsupportedEncodingException ex) {
-            Log.e(ex.getLocalizedMessage(), ex);
-        }
-    }
-
-    /**
-     * Initializes the logger that will append text to the console.
-     */
-    private void initLogger() {
-
-        Logger logger = Log.DEFAULT_LOGGER;
-
-        try {
-            final String decPath = AppUtils.getDecodedRootPath(getClass());
-            FileHandler handler = new FileHandler(decPath + "gzipper.log");
-            handler.setFormatter(new SimpleFormatter());
-            logger.addHandler(handler);
-            Log.setVerboseUiLogging(true);
-        } catch (IOException | SecurityException ex) {
-            logger.log(Level.SEVERE, null, ex);
-        }
-    }
-
-    /**
-     * Exits the application properly.
-     */
-    private void exitApplication() {
-        Log.i("Exiting application.", false);
-        Platform.exit();
-        System.exit(0);
     }
 
     /**
@@ -165,19 +231,20 @@ public class GZipper extends Application {
                 "java.util.logging.SimpleFormatter.format",
                 "[%1$tm-%1$te-%1$ty, %1$tH:%1$tM:%1$tS] %4$s: %5$s %n"
         );
+
         // store away settings file at application termination
         Runtime.getRuntime().addShutdownHook(new Thread(() -> {
             try {
                 Settings.getInstance().storeAway();
-                Logger logger = Log.DEFAULT_LOGGER;
+                final Logger logger = Log.DEFAULT_LOGGER;
                 for (Handler handler : logger.getHandlers()) {
                     handler.close();
                 }
-            }
-            catch (IOException ex) {
+            } catch (IOException ex) {
                 Log.e(ex.getLocalizedMessage(), ex);
             }
         }));
-        launch(args); // actually launch the UI
+
+        launch(args);
     }
 }
