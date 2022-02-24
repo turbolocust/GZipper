899f09c43b53ac166cc2e216516d2c491b2910eb
Matthias Fussenegger <turbolocust@users.noreply.github.com>
2022-02-24 15:02:54 +0100

    Update run_gzipper.bat
    Added missing modules to path

--- run_gzipper.bat
+++ run_gzipper.bat
@@ -1,2 +1,2 @@
 @echo off
-start /min PowerShell.exe -WindowStyle Hidden "java --module-path ".\lib" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,org.tukaani.xz -jar .\GZipper.jar"
+start /min PowerShell.exe -WindowStyle Hidden "java --module-path ".\lib" --add-modules=javafx.base,javafx.controls,javafx.fxml,javafx.graphics,org.apache.commons.compress,org.tukaani.xz,kotlin.stdlib -jar .\GZipper.jar"
